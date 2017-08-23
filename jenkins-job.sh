459add9bd4ae17160b5164df288537cad0b62655
Martin Jansa <Martin.Jansa@gmail.com>
2017-08-23 13:48:01 +0200

    jenkins-job.sh: update PACKAGECONFIG for qtbase (v1.8.27)
    * now with meta-luneui removed from world builds we need to add
      icu gl accessibility like it was added before by:
      meta-webos-ports/meta-luneui/recipes-qt/qt5/qtbase_git.bbappend
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_SCRIPT_VERSION="1.8.26"
+BUILD_SCRIPT_VERSION="1.8.27"
 BUILD_SCRIPT_NAME=`basename ${0}`
 
 # These are used by in following functions, declare them here so that
@@ -381,9 +381,7 @@ VIRTUAL-RUNTIME_apm = "fso-apm"
 # for qtwebkit etc
 # see https://bugzilla.yoctoproject.org/show_bug.cgi?id=5013
 # DEPENDS_append_pn-qtbase = " mesa"
-# now set with gles2 in meta-webos-ports/meta-luneui/recipes-qt/qt5/qtbase_git.bbappend
-# PACKAGECONFIG_append_pn-qtbase = " icu gl accessibility"
-PACKAGECONFIG_append_pn-qtbase = " freetype fontconfig"
+PACKAGECONFIG_append_pn-qtbase = " icu gl accessibility freetype fontconfig"
 
 # qtwayland doesn't like egl and xcomposite-glx enabled at the same time
 # http://lists.openembedded.org/pipermail/openembedded-devel/2016-December/110444.html
